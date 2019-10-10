32f2b7f0a12bd8df44dc319d8c49a98d08de29ab
Nepcam <cameron.nepe@gmail.com>
2019-10-10 21:48:09 +1300

    exec() method

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 let re;
 re = /hello/;
+
+// console.log(re);
+// console.log(re.source);
+
+// exec() - Return result in an array or null
+const result = re.exec('hello world');
+
+console.log(result);
+console.log(result[0]);
+console.log(result.index);
+console.log(result.input);
