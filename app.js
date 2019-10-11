7fd9e9e76b97e6c805281aae95db5ef6dd5ddb43
Nepcam <cameron.nepe@gmail.com>
2019-10-11 21:54:26 +1300

    test() exercise

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 let re;
-re = /hello/;
+re = /hello/i; // i = case insensitive
 
 // console.log(re);
 // console.log(re.source);
 
 // exec() - Return result in an array or null
-const result = re.exec('hello world');
+// const result = re.exec('hello world');
 
+// console.log(result);
+// console.log(result[0]);
+// console.log(result.index);
+// console.log(result.input);
+
+// test() - Returns true or false
+const result = re.test('Hello');
 console.log(result);
-console.log(result[0]);
-console.log(result.index);
-console.log(result.input);
