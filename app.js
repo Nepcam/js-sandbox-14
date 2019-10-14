c11beb9487bddc9eb55b000c008744277815681e
Nepcam <cameron.nepe@gmail.com>
2019-10-14 21:55:54 +1300

    match() - search() - replace() methods

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let re;
+re = /hello/;
 re = /hello/i; // i = case insensitive for a match
+// re = /hello/g; // g = Global search
 
 // console.log(re);
 // console.log(re.source);
@@ -13,5 +15,20 @@ re = /hello/i; // i = case insensitive for a match
 // console.log(result.input);
 
 // test() - Returns true or false
-const result = re.test('Hello');
-console.log(result);
+// const result = re.test('Hello');
+// console.log(result);
+
+// match() -Return result array or null
+// const str = 'Hello There';
+// const result = str.match(re);
+// console.log(result);
+
+// search() - Returns index of the first match if not found returns -1
+// const str = 'Cam Hello There';
+// const result = str.search(re);
+// console.log(result);
+
+// replace() - Return new string with some or all matches of a pattern
+const str = 'Hello There';
+const newStr = str.replace(re, 'Hi');
+console.log(newStr);
