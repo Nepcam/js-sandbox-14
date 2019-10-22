1cbeb4eccbff1634324bfc96d0acaa65681a600e
Nepcam <cameron.nepe@gmail.com>
2019-10-23 09:03:02 +1300

    brackets[] braces {} - Quantifiers and Parentheses()

--- symbols.js
+++ symbols.js
@@ -15,18 +15,27 @@ re = /gre?a?y/i;  // Optional character
 re = /gre?a?y\?/i;  // Escape character
 
 // Brackets [] - Character Sets
-re = /gr[ae]y/i;    // Must be an a or e
-re = /[GF]ray/i;    // Must be an G or F
-re = /[^GF]ray/i;   // Match anything except a G or F
-re = /[A-Z]ray/;    // Match any uppercase letter
-re = /[A-Za-z]ray/;    // Match any lowercase letter
+re = /gr[ae]y/i;        // Must be an a or e
+re = /[GF]ray/i;        // Must be an G or F
+re = /[^GF]ray/i;       // Match anything except a G or F
+re = /[A-Z]ray/;        // Match any uppercase letter
+re = /[a-z]ray/;        // Match any lowecase letter
+re = /[A-Za-z]ray/;     // Match any letter
+re = /[0-9][0-9]ray/;   // Match any digit or digits
 
+// Braces {} - Quantifiers
+re = /Hel{2}o/i;    // Must occur exactly {m} amount of times
+re = /Hel{2,4}o/i;    // Must occur exactly {m} amount of times
+re = /Hel{2,}o/i;    // Must occur exactly {m} times
+
+// Parentheses () - Grouping
+re = /^([0-9]x){3}$/
 
 // string to match
-const str = 'Aray';
+const str = '3x3x3x';
 
 // Log Results
-const result = re.exec(str);
+const result = re.exec(str);  // here we are calling the re(variable) along with the exec method and passing in the str(variable)
 console.log(result);
 
 // custom function to Log a result
